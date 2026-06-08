752c0ff6d3edd5b8f41fb7066830504f037ba9df
GhostHi1 <c-forsyth02@sky.com>
2026-06-08 19:56:56 +0100

    struct set up fo cli output input and exclude commands

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use clap::Parser;
 use std::path::PathBuf;
 
-
 #[derive(Parser)]
 struct Cli {
     #[arg(short = 'o', long)]
     output: PathBuf,
+
+    inputs: Vec<PathBuf>,
+
+    #[arg(short = 'e', long)]
+    exclude: Vec<PathBuf>,
 
 }
 
