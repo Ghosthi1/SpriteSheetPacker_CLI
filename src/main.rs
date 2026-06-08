bf87ecb3621b45788334929f424ffbb10366e877
GhostHi1 <c-forsyth02@sky.com>
2026-06-08 20:26:49 +0100

    Collects the pngs from a directory

--- src/main.rs
+++ src/main.rs
@@ -15,5 +15,28 @@ struct Cli {
 
 fn main() {
     let cli = Cli::parse();
+
+    let mut all_pngs = vec![];
+    for input in &cli.inputs {
+        all_pngs.extend(collect_pngs(input, &cli.exclude));
+    }
+    println!("{:#?}", all_pngs);
 }
 
+fn collect_pngs(path: &PathBuf, exclude: &Vec<PathBuf>) -> Vec<PathBuf> {
+    if path.extension() == Some(std::ffi::OsStr::new("png")) {
+        return vec![path.clone()];
+    }
+    if exclude.contains(&path) {
+        return vec![];
+    }
+    if path.is_dir() {
+        let mut results = vec![];
+        for entry in path.read_dir().unwrap() {
+            let entry_path = entry.unwrap().path();
+            results.extend(collect_pngs(&entry_path, exclude));
+        }
+        return results;
+    }
+    vec![]
+}
